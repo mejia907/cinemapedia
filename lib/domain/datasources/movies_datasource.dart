c618edbad59544280c716beba1b9e4440c889019
Andres Felipe Mejia <mejia9070@gmial.com>
2023-05-10 20:30:39 -0500

    create model movie

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -1,6 +1,6 @@
 ﻿import 'package:cinemapedia/domain/entities/movie.dart';
 
 /// Definir los origenes de datos y los metodos para traer la informacion
-abstract class MovieDatasource {
+abstract class MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1});
 }
